bffe911af31035bffc52dbbc672cbe1fc3221e86
fegimanam <ccaahh430@gmail.com>
2016-02-03 17:58:16 +0000

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,9 +1,9 @@
 local function run(msg)
-if msg.text == "hi" then
-	return "Hello bb"
+if msg.text == "سلام" then
+	return "؟سلام عشقم خوبی"
 end
-if msg.text == "Hi" then
-	return "Hello honey"
+if msg.text == "کیر" then
+	return "تو کسس ننت"
 end
 if msg.text == "Hello" then
 	return "Hi bb"
@@ -11,25 +11,34 @@ end
 if msg.text == "hello" then
 	return "Hi honey"
 end
+if msg.text == "کسس ننت" then
+	return "کیر آقام دست ننت"
+end
 if msg.text == "Salam" then
 	return "Salam aleykom"
 end
 if msg.text == "salam" then
 	return "va aleykol asalam"
 end
-if msg.text == "Samir" then
-	return "با باباییم چیکار داری"
+if msg.text == "ali" then
+	return "با باباییم چیکار داری؟"
 end
-if msg.text == "samir" then
-	return "با باباییم چیکار داری"
+if msg.text == "ناموسا" then
+	return "ننت و آقام چالوسن"
 end
-if msg.text == "SAMIR" then
-	return "با باباییم چیکار داری"
+if msg.text == "علی" then
+	return "علی بیا این پشمک کارت داره!
 end
-if msg.text == "Pulse" then
+if msg.text == "ZAC" then
+	return "Barash bezan sak"
+end
+if msg.text == "pob" then
 	return "Yes?"
 end
-if msg.text == "pulse" then
+if msg.text == "خوبی" then
+	return "فدات تو خوبی"
+end	
+if msg.text == "Pob" then
 	return "What?"
 end
 if msg.text == "bot" then
@@ -48,19 +57,27 @@ if msg.text == "bye" then
 	return "Bye Bye"
 end
 end
-
 return {
 	description = "Chat With Robot Server", 
 	usage = "chat with robot",
 	patterns = {
 		"^[Hh]i$",
 		"^[Hh]ello$",
-		"^[Ss]amir$",
-		"^SAMIR$",
+		"^[Zz]ac$",
+		"^ZAC$",
 		"^[Bb]ot$",
-		"^[Pp]ulse$",
+		"^[Uu]mbrella$",
 		"^[Bb]ye$",
 		"^?$",
+		"^سلام$",
+		"^خوبی$",
+		"^علی$",
+		"^کس ننت$",
+		"^ناموسا$",
+		"^ali$",
+		"^Ali$",
+		"^pob$",
+		"^کیر$",
 		"^[Ss]alam$",
 		}, 
 	run = run,
